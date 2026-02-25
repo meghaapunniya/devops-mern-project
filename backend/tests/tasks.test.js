3d582b59ab90cf21482badee889ebc436d8be5ef
meghaapunniya <meghaa2702@gmail.com>
2026-02-25 15:16:24 -0500

    Initial commit

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const {server, app} = require('../index')
+const mongoose = require('mongoose');
+
+describe('Test scenarios for TODO APIs at Backend', ()=>{
+    it('It should return 200 OK', async()=>{
+       const res = await request(app).get('/api/tasks')
+       expect(res.statusCode).toBe(200);
+    })
+    it('It should return an Object with the tasks property', async()=>{
+       const res = await request(app).get('/api/tasks')
+       expect(typeof (res.body)).toBe("object");
+       expect(res.body).toHaveProperty("tasks");
+    })
+})
+
+afterAll(async()=>{
+    await mongoose.connection.close();
+    await server.close();
+})
